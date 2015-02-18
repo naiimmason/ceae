77192961e6259aea5abeb7684b12c93ce3be1e87
Jeremy K <jj.of.keeler@gmail.com>
2015-02-18 11:29:07 -0500

    update favicon and gitignore

--- test-projects/input-test.py
+++ test-projects/input-test.py
@@ -5,6 +5,6 @@ def session(me):
   add(open("input-test.html"))
   add("<button type=\"button\" id=\"test-btn\" class=\"btn btn-success center-block\">Click Me</button>")
   take({"tag": "click", "id": "test-btn", "client": me})
-  add("<p class=\"center\">I've been clicked!</p>")
+  add("<p class=\"center\">I've been clicked!</p>", "#main-content")
 
 run(session)
