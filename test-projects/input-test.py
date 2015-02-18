2c22779ecf4268d207a0d3a073bb4ab366b9d9a7
Jeremy K <jj.of.keeler@gmail.com>
2015-02-18 10:09:48 -0500

    First commit

---
+++ test-projects/input-test.py
@@ -0,0 +1,10 @@
+from willow.willow import *
+
+def session(me):
+  let("Testing Input", "title")
+  add(open("input-test.html"))
+  add("<button type=\"button\" id=\"test-btn\" class=\"btn btn-success center-block\">Click Me</button>")
+  take({"tag": "click", "id": "test-btn", "client": me})
+  add("<p class=\"center\">I've been clicked!</p>")
+
+run(session)
