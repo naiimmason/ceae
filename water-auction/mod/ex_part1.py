cf7951278e86d2308bfd7a0c0fbe710795aedbe6
Jeremy Keeler <jj.of.keeler@gmail.com>
2015-03-04 13:07:50 -0500

    Add input validation for part A

--- water-auction/mod/ex_part1.py
+++ water-auction/mod/ex_part1.py
@@ -1,6 +1,13 @@
 from willow.willow import *
 import random as rand
 
+def isValid(input):
+  try:
+    truth = float(input) >= 0.0 and float(input) <= 5000.0
+    return truth
+  except ValueError:
+    return False
+
 def start(me, subj_id, waters):
   results = ["-1", "-1", "-1"]
   # Loop through all of the water choices
@@ -12,9 +19,13 @@ def start(me, subj_id, waters):
     let(temp_waters[i], "#waterID")
 
     # Wait for the client to submit an offer
-    # TODO: Add input checks
-    take({"tag": "click", "id": "submit", "client": me})
-    offer = peek("#offer")
+    offer = -1
+    while offer == -1:
+      take({"tag": "click", "id": "submit", "client": me})
+      offer = peek("#offer")
+      if not isValid(offer):
+        add("<button class=\"btn btn-lg btn-danger\" id=\"warningBtn\">Please provide a valid input</button><br><br>", "#warning")
+        offer = -1
     j = 0
     while j < len(waters):
       if temp_waters[i] == waters[j]:
