a3ffd3bd77d13fdb9beb9d83a22a07d8228e4095
Jeremy Keeler <jj.of.keeler@gmail.com>
2015-04-08 13:09:25 -0400

    update template

--- ar-cr/subject.py
+++ ar-cr/subject.py
@@ -1,4 +1,12 @@
 from willow.willow import *
+import reconnect
+import random as rand
 
 def start(me, subj_id, data_filepath1, survey_filepath1):
+  let("")
+  add(open("pages/subject.html"))
+
+  if reconnect.getPosition(subj_id) == "start":
+    pop("hidden", ".instructions")
+  
   return "hello"
