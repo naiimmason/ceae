38a674235112a4717df9ea60e3903a4a0c744fac
Jeremy Keeler <jj.of.keeler@gmail.com>
2015-04-10 13:42:54 -0400

    start initial mail server

---
+++ text-prototype/app.js
@@ -0,0 +1,34 @@
+var mailin = require("mailin");
+
+mailin.start({
+  port: 25,
+  disableWebhook: true // Disable the webhook posting.
+});
+
+/* Access simplesmtp server instance. */
+mailin.on('authorizeUser', function(connection, username, password, done) {
+  if (username == "johnsmith" && password == "mysecret") {
+    done(null, true);
+  } else {
+    done(new Error("Unauthorized!"), false);
+  }
+});
+
+/* Event emitted when a connection with the Mailin smtp server is initiated. */
+mailin.on('startMessage', function (connection) {
+  /* connection = {
+      from: 'sender@somedomain.com',
+      to: 'someaddress@yourdomain.com',
+      id: 't84h5ugf',
+      authentication: { username: null, authenticated: false, status: 'NORMAL' }
+    }
+  }; */
+  console.log(connection);
+});
+
+/* Event emitted after a message was received and parsed. */
+mailin.on('message', function (connection, data, content) {
+  console.log(data);
+  /* Do something useful with the parsed message here.
+   * Use parsed message `data` directly or use raw message `content`. */
+});
