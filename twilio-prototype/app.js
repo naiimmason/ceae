a0b8823090b836dde09c531648b781bddc7e8b08
Jeremy Keeler <jj.of.keeler@gmail.com>
2015-04-10 14:47:50 -0400

    rename twillo to twilio

---
+++ twilio-prototype/app.js
@@ -0,0 +1,28 @@
+var twilio = require("twilio");
+
+var auth = require("auth");
+
+var client = new twilio.RestClient(auth.sid, auth.token);
+
+client.sms.messages.create({
+    to:'+13025406713',
+    from: auth.number,
+    body:'ahoy hoy! Testing Twilio and node.js'
+}, function(error, message) {
+    // The HTTP request to Twilio will run asynchronously. This callback
+    // function will be called when a response is received from Twilio
+    // The "error" variable will contain error information, if any.
+    // If the request was successful, this value will be "falsy"
+    if (!error) {
+        // The second argument to the callback will contain the information
+        // sent back by Twilio for the request. In this case, it is the
+        // information about the text messsage you just sent:
+        console.log('Success! The SID for this SMS message is:');
+        console.log(message.sid);
+ 
+        console.log('Message sent on:');
+        console.log(message.dateCreated);
+    } else {
+        console.log('Oops! There was an error.');
+    }
+});
